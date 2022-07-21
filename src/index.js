3d7017f15c8aee5b5e8a4faca2c987d5809ccada
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-07-21 15:26:19 +0100

    Add get and post method

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import './style.scss';
-import data from './modules/Data.js';
+import { addScoreToApi, scoreList } from './modules/api';
+import List from './modules/List';
 
-const refresh = document.getElementById('referesh');
-refresh.addEventListener('click', () => {
-  window.location.reload();
+
+const referesh = document.getElementById('referesh');
+const username = document.getElementById('name');
+const score = document.getElementById('score');
+const add = document.getElementById('btn');
+const listItem = document.getElementsByClassName('item');
+
+
+referesh.addEventListener('click', () => {
+  if (!listItem.length) {
+    scoreList();
+    
+  }
 });
 
-const ScoreContainer = document.querySelector('.Scores');
-
-data.forEach((index) => {
-  const List = document.createElement('tr');
-  List.className = 'listScore';
-  ScoreContainer.appendChild(List);
-  const item = document.createElement('td');
-  item.className = 'itemScore';
-  item.textContent = index.name;
-  List.appendChild(item);
-  const item1 = document.createElement('td');
-  item1.className = 'itemScore';
-  item1.textContent = index.score;
-  List.appendChild(item1);
+add.addEventListener('click', () => {
+  const newItemIndex = listItem.length;
+  if (username.value && score.value && score.value <= 9999999) {
+    addScoreToApi(username.value, score.value);
+    List(username.value, score.value, newItemIndex);
+    username.value = null;
+    score.value = null;
+  } 
 });
---
+++ src/modules/List.js
@@ -0,0 +1,16 @@
+const ScoreContainer = document.querySelector('.Scores');
+const List = (user, score, index) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'item';
+    listItem.id = `item${index}`;
+    if (index === 0) {
+      listItem.innerHTML += `<span>${user}</span><span> ${score}</span> <i class="fa-solid fa-crown award"></i>`;
+    } else if (index === 1 || index === 2) {
+      listItem.innerHTML += `<span>${user}</span><span> ${score}</span> <i class="fa-solid fa-medal award"></i>`;
+    } else {
+      listItem.innerHTML += `<span>${user}</span><span> ${score}</span> <i class="fa-solid fa-star award"></i>`;
+    }
+    ScoreContainer.appendChild(listItem);
+  };
+  
+  export default List;
